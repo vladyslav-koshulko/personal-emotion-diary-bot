ad104034cf8863bdefb8ff8bb75f58fe7e2f3046
vladyslav <koshulkov@gmail.com>
2024-10-09 23:41:57 +0300

    Changed authentication method

--- src/main/java/sia/emotion_diary_bot/config/DriveConfiguration.java
+++ src/main/java/sia/emotion_diary_bot/config/DriveConfiguration.java
@@ -1,9 +1,10 @@
 package sia.emotion_diary_bot.config;
 
-import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.services.drive.Drive;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -12,13 +13,13 @@ import org.springframework.stereotype.Component;
 public class DriveConfiguration {
 
     private final JsonFactory jsonFactory;
-    private final Credential credential;
+    private final GoogleCredentials credential;
     private final NetHttpTransport netHttpTransport;
 
     @Value("${google.drive.app.name}")
     private String APPLICATION_NAME;
 
-    public DriveConfiguration(JsonFactory jsonFactory, Credential credential, NetHttpTransport netHttpTransport) {
+    public DriveConfiguration(JsonFactory jsonFactory, GoogleCredentials credential, NetHttpTransport netHttpTransport) {
         this.jsonFactory = jsonFactory;
         this.credential = credential;
         this.netHttpTransport = netHttpTransport;
@@ -30,7 +31,7 @@ public class DriveConfiguration {
         return new Drive.Builder(
                 netHttpTransport,
                 jsonFactory,
-                credential)
+                new HttpCredentialsAdapter(credential))
                 .setApplicationName(APPLICATION_NAME)
                 .build();
     }
--- src/main/java/sia/emotion_diary_bot/config/credentials/CredentialsConfiguration.java
+++ src/main/java/sia/emotion_diary_bot/config/credentials/CredentialsConfiguration.java
@@ -1,26 +1,16 @@
 package sia.emotion_diary_bot.config.credentials;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.DriveScopes;
+import com.google.auth.oauth2.GoogleCredentials;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import sia.emotion_diary_bot.services.GoogleDriveService;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.List;
 
@@ -29,39 +19,21 @@ public class CredentialsConfiguration {
     private static final Logger LOGGER = LoggerFactory.getLogger(GoogleDriveService.class);
 
     private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_FILE);
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
-    private final JsonFactory jsonFactory;
-    @Value("${credentials.path}")
-    private String CREDENTIALS_FILE_PATH;
-    @Value("${user.email}")
-    private String USER_EMAIL;
-    @Value("${server.port}")
-    private int SERVER_PORT;
-    @Value("${auth.receiver.host}")
-    private String RECEIVER_HOST;
 
-    public CredentialsConfiguration(JsonFactory jsonFactory) {
-        this.jsonFactory = jsonFactory;
-    }
+    @Value("${auth.service.account.key}")
+    private Resource ACCOUNT_KEY;
 
     @Bean
-    public Credential credential(final NetHttpTransport netHttpTransport) throws IOException {
-        LOGGER.debug("Credentials file: {}", CREDENTIALS_FILE_PATH);
+    public GoogleCredentials credential() {
         LOGGER.debug("Creating credentials...");
-        try (InputStream credentialsInputStream = getClass().getResourceAsStream(CREDENTIALS_FILE_PATH)) {
-            if (credentialsInputStream == null) {
-                LOGGER.error("Credentials file {} not found", CREDENTIALS_FILE_PATH);
-                throw new FileNotFoundException(CREDENTIALS_FILE_PATH);
-            }
-            LOGGER.info("Credentials loaded.");
-            GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(jsonFactory, new InputStreamReader(credentialsInputStream));
-            GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                    netHttpTransport, jsonFactory, clientSecrets, SCOPES)
-                    .setDataStoreFactory(new FileDataStoreFactory(new File(TOKENS_DIRECTORY_PATH)))
-                    .setAccessType("offline")
-                    .build();
-            LocalServerReceiver receiver = new LocalServerReceiver.Builder().setHost(RECEIVER_HOST).setPort(SERVER_PORT).build();
-            return new AuthorizationCodeInstalledApp(flow, receiver).authorize(USER_EMAIL);
+        try {
+            GoogleCredentials credentials = GoogleCredentials.fromStream(ACCOUNT_KEY.getInputStream()).createScoped(SCOPES);
+            LOGGER.debug("Credentials loaded successfully");
+            return credentials;
+        } catch (IOException e) {
+            LOGGER.error("Could not create credentials.", e);
+            LOGGER.warn("Credentials: {}", ACCOUNT_KEY.toString());
+            throw new RuntimeException(e);
         }
     }
 }
