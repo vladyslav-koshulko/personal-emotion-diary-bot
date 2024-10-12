adf86dc1900dc60e23a5c8dc5a2ef40b7e45e594
vladyslav <koshulkov@gmail.com>
2024-10-13 00:20:30 +0300

    Added clearing webhook

--- src/main/java/sia/emotion_diary_bot/config/BeanConfiguration.java
+++ src/main/java/sia/emotion_diary_bot/config/BeanConfiguration.java
@@ -8,6 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -27,6 +30,16 @@ public class BeanConfiguration {
         try {
             return GoogleNetHttpTransport.newTrustedTransport();
         } catch (GeneralSecurityException | IOException e) {
+            LOGGER.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Bean
+    public TelegramBotsApi telegramBotsApi() {
+        try {
+            return new TelegramBotsApi(DefaultBotSession.class);
+        } catch (TelegramApiException e) {
             LOGGER.error(e.getMessage());
             throw new RuntimeException(e);
         }
--- src/main/java/sia/emotion_diary_bot/services/TelegramBotRegistrarService.java
+++ src/main/java/sia/emotion_diary_bot/services/TelegramBotRegistrarService.java
@@ -1,33 +1,53 @@
 package sia.emotion_diary_bot.services;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.updates.DeleteWebhook;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Component
 public class TelegramBotRegistrarService {
     private static final Logger LOGGER = LoggerFactory.getLogger(MessageService.class);
 
     private final MessageService messageService;
+    private final TelegramBotsApi telegramBotsApi;
 
-    public TelegramBotRegistrarService(MessageService messageService) {
+    public TelegramBotRegistrarService(MessageService messageService, TelegramBotsApi telegramBotsApi) {
         this.messageService = messageService;
+        this.telegramBotsApi = telegramBotsApi;
     }
 
 
     @EventListener(ApplicationReadyEvent.class)
     public void init() {
         try {
-            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(messageService);
         } catch (TelegramApiException e) {
             LOGGER.error(e.getMessage());
             throw new RuntimeException(e);
         }
     }
+
+
+    @PreDestroy
+    @PostConstruct
+    public void clearWebhook() {
+        try {
+            LOGGER.debug("Removing webhook...");
+            DeleteWebhook deleteWebhook = new DeleteWebhook();
+            messageService.execute(deleteWebhook);
+            LOGGER.debug("Webhook removed.");
+        } catch (TelegramApiException e) {
+            LOGGER.debug("Webhook could not be removed.");
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
